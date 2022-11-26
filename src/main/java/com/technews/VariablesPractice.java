79cae938565fd0ee491741885fd4c6d0937738e3
Demi Hayashi <demi.h@me.com>
2022-11-26 12:58:21 -0800

    create the Entity models

---
+++ src/main/java/com/technews/VariablesPractice.java
@@ -0,0 +1,15 @@
+package com.technews;
+
+public class VariablesPractice {
+    public static void main(String[] args) {
+        int a = 50;
+        double b = 50.99;
+        double sum = a + b;
+
+        String c = "cat";
+        String concat = c + sum;
+
+        System.out.println(sum);
+        System.out.println(concat);
+    }
+}
